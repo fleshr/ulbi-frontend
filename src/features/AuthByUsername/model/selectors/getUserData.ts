08c19fa2490a721a8e5b5529fcb0d72f9e460198
fleshr <sfrxfleshr@gmail.com>
2025-04-19 21:59:06 +0500

    36

--- src/features/AuthByUsername/model/selectors/getUserData.ts
+++
@@ -1,1 +0,0 @@
-export const getUserData = (state: RootState) => state.login;
---
+++ src/features/AuthByUsername/model/services/loginByUsername.test.ts
@@ -0,0 +1,42 @@
+import { Dispatch } from "@reduxjs/toolkit";
+import { loginByUsername } from "./loginByUsername";
+import axios from "axios";
+import { setUser } from "@/entities/User";
+
+jest.mock("axios");
+const mockedAxios = jest.mocked(axios);
+
+describe("loginByUsername", () => {
+  it("succees", async () => {
+    mockedAxios.post.mockResolvedValue(
+      Promise.resolve({ data: { id: "1", username: "123" } }),
+    );
+    const dispatch: Dispatch = jest.fn();
+    const getState: () => unknown = jest.fn();
+    const action = loginByUsername({ username: "123", password: "123" });
+    const res = await action(dispatch, getState, undefined);
+
+    expect(dispatch).toHaveBeenCalledWith(
+      setUser({ id: "1", username: "123" }),
+    );
+    expect(dispatch).toHaveBeenCalledTimes(3);
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(mockedAxios.post).toHaveBeenCalledTimes(1);
+    expect(res.payload).toEqual({ id: "1", username: "123" });
+    expect(res.meta.requestStatus).toBe("fulfilled");
+  });
+
+  it("error", async () => {
+    mockedAxios.post.mockResolvedValue(Promise.resolve({ status: 403 }));
+    const dispatch: Dispatch = jest.fn();
+    const getState: () => unknown = jest.fn();
+    const action = loginByUsername({ username: "123", password: "123" });
+    const res = await action(dispatch, getState, undefined);
+
+    expect(dispatch).toHaveBeenCalledTimes(2);
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(mockedAxios.post).toHaveBeenCalledTimes(1);
+    expect(res.payload).toBe("error");
+    expect(res.meta.requestStatus).toBe("rejected");
+  });
+});
